036f9792627a494b260ee3c93f562c55e5889a13
george-shammar <george.gbenle@outlook.com>
2021-04-16 22:20:45 +0000

    Add validation for password

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :username, presence: true, length: { maximum: 20 }
-
+  validates :password, presence: true, length: { minimum: 6 }
 
   has_many :posts, class_name: 'Post', foreign_key: 'user_id', dependent: :destroy
 end

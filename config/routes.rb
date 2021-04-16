3f7fd62c07e89a03b7748c1b0c20c610cd616cba
george-shammar <george.gbenle@outlook.com>
2021-04-16 22:14:24 +0000

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
   resources :posts
+
+  resources :users, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'posts#index'
 end

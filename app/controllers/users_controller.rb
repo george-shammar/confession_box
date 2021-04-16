5cf76d769648f4b3bd656e02b13cbc869cefbf4b
george-shammar <george.gbenle@outlook.com>
2021-04-16 22:02:01 +0000

    update controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   
     def show
       @user = User.find(params[:id])
-      @opinion = Opinion.new
+      @post = Post.new
     end
   
     def follow

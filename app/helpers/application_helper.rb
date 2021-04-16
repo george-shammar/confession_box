6e198c88feb79d1dac2d3f6991ccb7df6b412a6b
george-shammar <george.gbenle@outlook.com>
2021-04-16 22:25:54 +0000

    Create navbar sign out button

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def navbar_button_sign_out
+    if user_signed_in?
+      link_to 'Sign out', destroy_user_session_path, method: :delete,
+                                                     class: 'btn btn-light mx-1 btn-sm'
+    end
+  end
 end

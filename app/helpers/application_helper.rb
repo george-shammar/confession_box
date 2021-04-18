52f0d26320b2a1edb417bbaa78bc60dbfc774b1a
george-shammar <george.gbenle@outlook.com>
2021-04-18 15:15:07 +0000

    Display user log in username

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,12 @@ module ApplicationHelper
   def navbar_button_log_in
     link_to 'Sign in', new_user_session_path, class: 'btn btn-light mx-1 btn-sm' unless user_signed_in?
   end
+
+  def signed_in_as
+    if user_signed_in?
+      link_to current_user.username, user_path
+    else
+      linkt_to 'Guest', new_user_registration_path
+    end
+  end
 end

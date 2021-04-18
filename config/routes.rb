f7aa122e6f2178db2c5aa0406b8799933fd8e74d
george-shammar <george.gbenle@outlook.com>
2021-04-18 22:31:01 +0000

    Create comments route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   resources :posts do
     resources :likes, only: [:create, :destroy]
+    resources :comments, only: [:create]
   end
 
   resources :users, only: [:index, :show]

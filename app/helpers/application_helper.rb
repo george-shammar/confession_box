b38ac306f573b8422d81f5d7bc09fdac0e501943
george-shammar <george.gbenle@outlook.com>
2021-04-18 16:26:07 +0000

    Add bootstrap and font awesome

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,13 +2,13 @@ module ApplicationHelper
 
   def navbar_button_sign_out
     if user_signed_in?
-      link_to 'Sign out', destroy_user_session_path, method: :delete,
-                                                     class: 'btn btn-light mx-1 btn-sm'
+      link_to 'Sign out', destroy_user_session_path, method: :delete
     end
   end
 
   def navbar_button_log_in
-    link_to 'Sign in', new_user_session_path, class: 'btn btn-light mx-1 btn-sm' unless user_signed_in?
+    link_to 'Sign in', new_user_session_path, unless user_signed_in?
+  end
   end
 
   def signed_in_as

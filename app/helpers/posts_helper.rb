a5f4a441fa776eefe9cb95e837d91e4152c4dc25
george-shammar <george.gbenle@outlook.com>
2021-04-18 20:33:38 +0000

    create likes button

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,11 @@
 module PostsHelper
+    def like_or_dislike_btn(post)
+        like = Like.find_by(post: post, user: current_user)
+        if like
+          link_to('Likes!', post_like_path(id: like.id, post_id: post.id),
+                  method: :delete)
+        else
+          link_to('Likes!', post_likes_path(post_id: post.id), method: :post)
+        end
+      end
 end
